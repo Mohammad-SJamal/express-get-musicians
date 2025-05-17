95a3ab937c9fee839a2c92399de542fa6ff33475
Jamal <jamamo7@verizon.com>
2025-05-17 16:44:32 -0500

    finished musician CRUD endpoints, and added tests

--- index.test.js
+++ index.test.js
@@ -12,6 +12,39 @@ const {seedMusician} = require("./seedData");
 
 describe('./musicians endpoint', () => {
     // Write your tests here
+    test("Testing musicians endpoint", async () => {
+        const response = await request(app).get("/musicians");
+        expect(response.statusCode).toBe(200);
+        const mscns = await Musician.findAll();
+        expect(mscns.length).toEqual(JSON.parse(response.text).length);
+    })
+
+    test("Testing musicians parameters endpoint", async () => {
+        const response = await request(app).get("/musicians/1");
+        expect(response.statusCode).toBe(200);
+        const mscn = await Musician.findByPk(1);
+        expect(mscn.name).toBe(JSON.parse(response.text).name);
+    })
+
+    test("Testing if can add new musicians", async () => {
+        const response = await request(app).post("/musicians/add/john/triangle");
+        expect(response.statusCode).toBe(200);
+        expect((await Musician.findOne({ where : {
+            name: "john"
+        }})).name).toBe(JSON.parse(response.text).name);
+    })
+
+    test("Testing if post can change an entry", async () => {
+        const response = await request(app).post("/musicians/1/marcel/guitar");
+        expect(response.statusCode).toBe(200);
+        expect((await Musician.findByPk(1)).name).toBe("marcel");
+    })
+
+    test("Testing if can delete something from database", async () => {
+        const response = await request(app).post("/musicians/delete/3");
+        expect(response.statusCode).toBe(200);
+        expect(await Musician.findByPk(3)).toBe(null);
+    })
     
     
 
